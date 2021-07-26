869910e8e5dae88a6f9c0e55bc650db32d493633
reedgrlu <51003043+reedgrlu@users.noreply.github.com>
2021-07-25 22:26:26 -0700

    renamed project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header(){
-    return (<header className="d-flex justify-content-center"><h1>Currency Exchange</h1></header>)
+    return (<header className="d-flex justify-content-center"><h1>Currency Converter</h1></header>)
 }
 
 export default Header;
